c5690496b64171a64a80afabb40c576704ef65cb
Nguyen Tran <nguyen.tnc1507@gmail.com>
2023-07-27 22:07:31 +0700

    add packages

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/Button';
+
+export default function Test() {
+  return (
+    <div className='p-10'>
+      <Button>Connect Wallet</Button>
+    </div>
+  );
+}
